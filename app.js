60cc11fb272120c35987d76c238239f35139dc03
sasanka <sasankasahoo2020@outlook.com>
2022-11-04 18:40:32 +0530

    some changes in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const productRoutes = require('./routes/product.route');
 const authRoutes = require('./routes/auth.route');
 const roleRoutes = require('./routes/role.route');
 const orderRoutes = require('./routes/order.route');
-const serverPort = 8080
+const serverPort = 8080;
 const app = express();
 app.use(cors());
 
@@ -21,8 +21,12 @@ authRoutes(app);
 roleRoutes(app);
 orderRoutes(app);
 
-app.listen( serverPort, async () => {
-    await sequelize.sync();
+// app.listen( serverPort, async () => {
+//     await sequelize.sync();
+//     console.log(`App listening on ${serverPort}`)
+// })
+
+app.listen( serverPort, () => {
     console.log(`App listening on ${serverPort}`)
 })
 
