c7815404b8102bd6ca090b4651ce0129cc02123a
sasanka <sasankasahoo2020@outlook.com>
2022-11-04 23:32:57 +0530

    app.js file changed

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ authRoutes(app);
 roleRoutes(app);
 orderRoutes(app);
 
-// app.listen( serverPort, async () => {
-//     await sequelize.sync();
-//     console.log(`App listening on ${serverPort}`)
-// })
-
-app.listen( serverPort, () => {
+app.listen( serverPort, async () => {
+    await sequelize.sync();
     console.log(`App listening on ${serverPort}`)
 })
+
+// app.listen( serverPort, () => {
+//     console.log(`App listening on ${serverPort}`)
+// })
 
